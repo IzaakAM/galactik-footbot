9446d4b4de9a780ad30fbdf4af5b57e592512b8d
IzaakAM <a.izaak.m@gmail.com>
2024-06-09 20:35:20 +0200

    feat(windows) : add photos and animations

--- galactik-footbot/src/components/informations.jsx
+++ galactik-footbot/src/components/informations.jsx
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const InformationModal = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+import Izaak from '../assets/izaak.jpeg';
+import Yasmine from '../assets/yasmine.png';
+import Yann from '../assets/yann.jpeg';
+import Amine from '../assets/amine.jpeg';
 
-  return createPortal(
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
-      <div className="bg-gray-900 border border-gray-300 rounded-lg p-6 w-full max-w-xl h-full max-h-[500px] flex flex-col">
-        <p className="text-2xl mb-4 bg-gray-600 border border-gray-300 rounded-lg p-6 w-full max-w-xl h-full max-h-[500px] flex flex-col">
-          Ce projet consiste à créer un chatbot pour répondre aux questions des utilisateurs sur le foot à l'international. Il est réalisé dans le cadre du cours de projet de 4A à l'INSA de l'ambiance par Izaak, Yasmine, Yann et Amine.
-        </p>
-        <div className="flex justify-center mt-4">
-          <button
-            className="bg-gray-900 text-white px-4 py-2 rounded hover:opacity-75 transition-opacity duration-300"
-            onClick={onClose}
-          >
-            Fermer
-          </button>
+
+
+  const InformationModal = ({ isOpen, onClose }) => {
+    const [animationClass, setAnimationClass] = useState('');
+
+    const teamMembers = [
+        { name: 'Izaak', image: Izaak, linkedin: 'https://www.linkedin.com/in/izaak-aubert-m%C3%A9cibah/' },
+        { name: 'Yasmine', image: Yasmine, linkedin: 'https://www.linkedin.com/in/yasmine-el-mimouni/' },
+        { name: 'Yann', image: Yann, linkedin: 'https://www.linkedin.com/in/yann-verkimpe-43437b234/' },
+        { name: 'Amine', image: Amine, linkedin: 'https://www.linkedin.com/in/amine-benayada-4997111a9/' },
+      ];
+  
+    useEffect(() => {
+      if (isOpen) {
+        setAnimationClass('opacity-25 scale-75');
+        setTimeout(() => {
+          setAnimationClass('opacity-100 scale-100');
+        }, 1);
+      } else {
+        setAnimationClass('opacity-25 scale-75');
+        setTimeout(onClose, 300);
+      }
+    }, [isOpen, onClose]);
+  
+    if (!isOpen) return null;
+  
+    return createPortal(
+      <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50 transition-opacity duration-300">
+        <div className={`bg-gray-900 border border-gray-300 rounded-lg p-6 w-full max-w-3xl h-grow max-h-[500px] flex flex-col space-y-[20px] transform transition-transform duration-300 ${animationClass}`}>
+          <p className="text-xl bg-gray-600 border border-gray-300 rounded-lg p-4 h-full text-center">
+            Ce projet consiste à créer un chatbot pour répondre aux questions des utilisateurs sur le foot à l'international. Il est réalisé dans le cadre du cours de projet de 4A à l'INSA de l'ambiance par Izaak, Yasmine, Yann et Amine.
+          </p>
+          <div className="flex justify-around">
+            {teamMembers.map((member) => (
+              <a
+                href={member.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex flex-col items-center hover:bg-gray-700 p-4 rounded-lg space-y-[12px] transition duration-300"
+                key={member.name}
+              >
+                <img src={member.image} alt={member.name} className="rounded-full w-28 h-28" />
+                <span className="text-white text-lg">{member.name}</span>
+              </a>
+            ))}
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="bg-gray-600 border border-gray-300 text-white px-4 py-2 rounded-lg hover:bg-red-500 transition-colors duration-300"
+              onClick={onClose}
+            >
+              Fermer
+            </button>
+          </div>
         </div>
-      </div>
-    </div>,
-    document.body
-  );
-};
-
-export default InformationModal;
+      </div>,
+      document.body
+    );
+  };
+  
+  export default InformationModal;
--- galactik-footbot/src/components/suggestion.jsx
+++ galactik-footbot/src/components/suggestion.jsx
@@ -1,10 +1,9 @@
-// src/components/SuggestionModal.jsx
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const SuggestionModal = ({ isOpen, onClose, onSend }) => {
   const [message, setMessage] = useState('');
+  const [animationClass, setAnimationClass] = useState('');
 
   const handleChange = (event) => {
     setMessage(event.target.value);
@@ -17,27 +16,39 @@ const SuggestionModal = ({ isOpen, onClose, onSend }) => {
     }
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      setAnimationClass('opacity-25 scale-75');
+      setTimeout(() => {
+        setAnimationClass('opacity-100 scale-100');
+      }, 100);
+    } else {
+      setAnimationClass('opacity-25 scale-75');
+      setTimeout(onClose, 300);
+    }
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   return createPortal(
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
-      <div className="bg-gray-900 border border-gray-300 rounded-lg p-6 w-full max-w-xl h-full max-h-[500px] flex flex-col">
-        <p className="text-2xl mb-4">Envoyer une suggestion</p>
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50 transition-opacity duration-300">
+      <div className={`bg-gray-900 border border-gray-300 rounded-lg p-6 w-full max-w-xl h-[500px] space-y-[20px] flex flex-col transform transition-transform duration-300 ${animationClass}`}>
+        <p className="text-2xl">Envoyer une suggestion</p>
         <textarea
           className="w-full flex-grow p-2 border border-gray-300 rounded bg-gray-600 resize-none"
           placeholder="Écrivez votre suggestion ici..."
           value={message}
           onChange={handleChange}
         ></textarea>
-        <div className="flex justify-end mt-4">
+        <div className="flex justify-end space-x-[20px]">
           <button
-            className="bg-gray-900 text-white px-4 py-2 rounded hover:opacity-75 transition-opacity duration-300"
+            className="bg-gray-600 border border-gray-300 text-white rounded-lg hover:bg-red-500 transition-colors duration-300 px-4 py-2"
             onClick={onClose}
           >
             Fermer
           </button>
           <button
-            className={`bg-blue-600 text-white px-4 py-2 ml-2 rounded hover:opacity-75 transition-opacity duration-300 ${message.trim() === '' ? 'opacity-50 cursor-not-allowed' : ''}`}
+            className={`bg-gray-600 hover:bg-green-600 border border-gray-300 rounded-lg text-white transition-colors duration-300 px-4 py-2 ${message.trim() === '' ? 'opacity-50 cursor-not-allowed' : ''}`}
             onClick={handleSend}
             disabled={message.trim() === ''}
           >
